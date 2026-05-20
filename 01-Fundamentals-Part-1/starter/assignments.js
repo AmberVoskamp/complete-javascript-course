b5a86519a82173f3e557cff66f77ebca8fe4b9b7
AmberVoskamp <amber@voskamp.tv>
2026-05-20 11:25:30 +0200

    Lesson 11

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+let country = "Netherlands";
+let continent = "Europe";
+let population = 17.4; // in millions
+let isIsLand = false;
+let laguage;
+
+console.log(isIsLand);
+console.log(population);
+console.log(country);
+console.log(laguage);
