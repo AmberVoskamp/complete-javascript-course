db5e80e34bc293d0714661aa57e57096d131eda8
AmberVoskamp <amber@voskamp.tv>
2026-05-27 09:42:49 +0200

    Lesson 14

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -96,3 +96,18 @@ console.log(isFullAge);
 
 console.log(now - 2000 > now - 2018);
 */
+
+const now = 2036;
+const ageJonas = now - 2000;
+const ageSarah = now - 2018;
+
+console.log(now - 2000 > now - 2018);
+
+console.log(25-10-5);
+
+let x, y;
+x = y = 25 - 10 - 5; //x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
