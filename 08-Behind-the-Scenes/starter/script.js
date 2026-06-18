fc0cbe8739f7b2ee3a9890b2a245444cf39a3c79
AmberVoskamp <amber@voskamp.tv>
2026-06-18 16:21:50 +0200

    Lesson 99

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   function printAge() {
@@ -34,3 +35,48 @@ const firstName = 'Amber';
 calcAge(1991);
 //console.log(age);
 //printAge();
+*/
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Amber'; // hoisted to undefined
+let job = 'teacher';
+const year = 2000;
+
+//Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) {
+  deleteShoppingCart();
+}
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
